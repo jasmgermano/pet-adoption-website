07eddd10707ba5c81b15222dcf8eaa9e069a480d
jasmine <jasmigermano@gmail.com>
2024-07-10 18:32:29 -0300

    melhorando footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,13 @@ export function Footer() {
     return (
         <footer className="flex justify-center mb-4">
             <Container>
-                <div className="flex flex-col items-center justify-center">
-                    <p className="text-center font-semibold text-sm">Desenvolvido com amor por <span className="block"><a href="https://www.linkedin.com/in/jasmgermano/" target="_blank" rel="noreferrer" className="text-custom-red">Jasmine Germano</a></span></p>
+                <div className="flex items-center flex-col">
+                    <span>
+                        <a className="inline-block mr-4 cursor-pointer text-lg font-bold text-custom-cyan whitespace-nowrap" href="/">Adotar é legAU</a>
+                    </span>
+                    <div className="">
+                        <p className="text-center font-semibold text-sm">Desenvolvido com 💜 por <a href="https://www.linkedin.com/in/jasmgermano/" target="_blank" rel="noreferrer" className="text-custom-red">Jasmine Germano</a></p>
+                    </div>
                 </div>
             </Container>
         </footer>
